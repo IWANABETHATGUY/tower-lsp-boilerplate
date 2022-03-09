a604cd6075c2b950e31228f28c7ee238a24abd48
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-03-09 12:35:50 +0800

    refactor: 💡 remove some comment

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -41,13 +41,6 @@ let client: LanguageClient;
 // type a = Parameters<>;
 
 export async function activate(context: ExtensionContext) {
-  // The server is implemented in node
-  // let serverModule = context.asAbsolutePath(
-  // 	path.join('server', 'out', 'server.js')
-  // );
-  // The debug options for the server
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  // let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
   const traceOutputChannel = window.createOutputChannel("Nrs Language Server trace");
   const command = process.env.SERVER_PATH || "nrs-language-server";
@@ -81,42 +74,7 @@ export async function activate(context: ExtensionContext) {
   // Create the language client and start the client.
   client = new LanguageClient("nrs-language-server", "nrs language server", serverOptions, clientOptions);
   activateInlayHints(context);
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  // TODO: enable ts language server
-  // const typescriptServerOptions: ServerOptions = {
-  //   run: { module: typescriptServerModule, transport: TransportKind.ipc },
-  //   debug: {
-  //     module: typescriptServerModule,
-  //     transport: TransportKind.ipc,
-  //     options: debugOptions,
-  //   },
-  // };
-
-  // Options to control the language client
-  // const typescriptClientOptions: LanguageClientOptions = {
-  //   // Register the server for plain text documents
-  //   documentSelector: [
-  //     { scheme: "file", language: "typescript" },
-  //     { scheme: "file", language: "typescriptreact" },
-  //   ],
-  //   synchronize: {
-  //     // Notify the server about file changes to '.clientrc files contained in the workspace
-  //     fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-  //   },
-  // };
-  // let tsClient = new LanguageClient(
-  //   "tjs-postfix-ts",
-  //   "TJS Language Server ts",
-  //   typescriptServerOptions,
-  //   typescriptClientOptions
-  // );
-
-  // Create the language client and start the client.
-  // Start the client. This will also launch the server
-
-  client.start();
-  // tsClient.start();
+   client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
