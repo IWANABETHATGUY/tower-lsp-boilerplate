f62744fcbca577eecdc83987a191a0f352ef80b6
IWANABETHATGUY <iwanabethatguy@qq.com>
2026-01-15 00:11:48 +0800

    Rename nrs-language-server to l-language-server and update related configurations (#39)

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,7 @@ impl LanguageServer for Backend {
                             text_document_registration_options: {
                                 TextDocumentRegistrationOptions {
                                     document_selector: Some(vec![DocumentFilter {
-                                        language: Some("nrs".to_string()),
+                                        language: Some("l".to_string()),
                                         scheme: Some("file".to_string()),
                                         pattern: None,
                                     }]),
